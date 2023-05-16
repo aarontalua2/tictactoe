3c132b1b24300893930488b139c357e806cae2f2
aarontalua2 <aarontalua14@gmail.com>
2023-05-17 03:01:32 +0800

    Now working -- only 2 player mode -- no animations

--- script.js
+++ script.js
@@ -1,144 +1,158 @@
 // GameBoard Module
 const GameBoard = (() => {
-   
-    const board = [];
+	const board = new Array(9).fill(undefined);
+	const getBoard = () => board;
 
-    const resetBoard = (() => {
-            for (i=0; i<9; i++) {
-                board[i] = undefined;
-        } 
-    })();
+	const resetBoard = () => {
+		for (i = board.length - 1; i >= 0; i--) {
+			board[i] = undefined;
+		}
+	};
 
-    const getBoard = () => board;
+	// Fills boardArray ONLY
+	const selectSlot = (index, player) => {
+		board[index] = player.getMark();
+	};
 
-    const selectSlot = (index, mark) => {
-        
-        if(board[index] == undefined) {
-            board[index] = mark;
-            DisplayController.updateBoardDisplay();
-        }
-    }
-    
-    return {getBoard, selectSlot}
-
+	return { getBoard, selectSlot, resetBoard };
 })();
 
 // Player Factory
-const Player = (mark) => {
-    const getMark = () => mark;
+const Player = (name, mark) => {
+	const getName = () => name;
+	const getMark = () => mark;
 
-    return {getMark}
-}
+	return { getName, getMark };
+};
 
-
-
-
-// GameController
-
+// Game Controller -- controls the flow of the game
 const GameController = (() => {
-    const Player1 = Player('x');
-    const Player2 = Player('o');
+	const Player1 = Player("One", "❌");
+	const Player2 = Player("Two", "⭕");
+	let activePlayer = Player1;
 
-    let ActivePlayer = Player1;
+	const getActivePlayer = () => activePlayer;
+	const switchActivePlayer = () => {
+		activePlayer = activePlayer === Player1 ? Player2 : Player1;
+	};
 
-    let getActivePlayer = () => ActivePlayer;
-    const switchActivePlayer = () => {
-        ActivePlayer = ActivePlayer === Player1 ? Player2 : Player1
-    }
+	const checkWin = () => {
+		const board = GameBoard.getBoard();
+		const allEqual = (arr) =>
+			arr.every((val) => val === arr[0] && val != undefined);
 
-    const checkWin = () => {
-        const board = GameBoard.getBoard();
-        const allEqual = arr => arr.every(val => (val === arr[0] && val != undefined));
+		// For checking draw
+		const allMarked = board.every((val) => val !== undefined);
 
-        // Winning combinations
-        const hor1 = allEqual(board.slice(0,3));
-        const hor2 = allEqual(board.slice(3,6));
-        const hor3 = allEqual(board.slice(6,9));
-        const ver1 = allEqual([board[0],board[3],board[6]]);
-        const ver2 = allEqual([board[1],board[4],board[7]]);
-        const ver3 = allEqual([board[2],board[5],board[8]]);
-        const diag1 = allEqual([board[0],board[4],board[8]]);
-        const diag2 = allEqual([board[2],board[4],board[6]]);
-        const winConditions = [hor1, hor2, hor3, ver1, ver2, ver3, diag1, diag2];
-        const foundWin = winConditions.includes(true);
+		// Winning combinations
+		const hor1 = allEqual(board.slice(0, 3));
+		const hor2 = allEqual(board.slice(3, 6));
+		const hor3 = allEqual(board.slice(6, 9));
+		const ver1 = allEqual([board[0], board[3], board[6]]);
+		const ver2 = allEqual([board[1], board[4], board[7]]);
+		const ver3 = allEqual([board[2], board[5], board[8]]);
+		const diag1 = allEqual([board[0], board[4], board[8]]);
+		const diag2 = allEqual([board[2], board[4], board[6]]);
+		const winConditions = [hor1, hor2, hor3, ver1, ver2, ver3, diag1, diag2];
+		const foundWin = winConditions.includes(true);
 
-        if (foundWin) {
-            // Stop game, show winner 
-            console.log('winner found')
-            DisplayController.removeEventListeners();
-        } else {
-            console.log('no winners yet')
-            switchActivePlayer()
-        }
+		// If there's a winner
+		if (foundWin) {
+			DisplayController.showWinner();
+		}
 
-    }
+		// If draw
+		else if (allMarked) {
+			DisplayController.showDraw();
+		}
 
-    const playRound = (e) => {
-        index = e.target.getAttribute('data-index');
-        mark = ActivePlayer.getMark();
-        GameBoard.selectSlot(index, mark);
+		// No winner and not draw
+		else {
+			console.log("no winner, not draw");
+			switchActivePlayer();
+			DisplayController.showTurn();
+		}
+	};
 
-        checkWin();
-    }
+	const playerMove = (e) => {
+		const index = e.target.getAttribute("data-index");
+		const board = GameBoard.getBoard();
 
+		if (board[index] == undefined) {
+			GameBoard.selectSlot(index, activePlayer);
+			DisplayController.addMark(index, activePlayer);
+			checkWin();
+		}
+	};
 
-    return {switchActivePlayer, getActivePlayer, playRound}
+	const restartGame = () => {
+		activePlayer = Player1;
+		GameBoard.resetBoard();
+		DisplayController.initializeHTML();
+	};
 
-})()
-
+	return { getActivePlayer, playerMove, restartGame };
+})();
 
 // Display Controller
 const DisplayController = (() => {
-    const gameboard = document.querySelector('.gameboard');
+	const HTMLPlayer = document.querySelector(".player-turn");
+	const restartBtn = document.querySelector("#restart");
+	const boardArray = GameBoard.getBoard();
+	let currentIndex = 0;
 
+	const initializeHTML = () => {
+		const HTMLBoard = document.querySelector(".gameboard");
+		HTMLBoard.innerHTML = ``;
+		boardArray.forEach((item) => {
+			const newItem = document.createElement("button");
+			newItem.classList.add("board-item");
+			newItem.setAttribute("data-index", currentIndex);
+			HTMLBoard.append(newItem);
+			newItem.addEventListener("click", GameController.playerMove);
+			currentIndex++;
+		});
+		showTurn();
+		currentIndex = 0;
+	};
 
-    const updateBoardDisplay = () => {
-        gameboard.innerHTML = ``;
-        const board = GameBoard.getBoard();
-        currentIndex = 0;
+	const addMark = (index, player) => {
+		const target = document.querySelector(`[data-index="${index}"]`);
+		target.innerHTML = `<p>${player.getMark()}</p>`;
+	};
 
-        board.forEach(element => {
+	const showTurn = () => {
+		const activePlayer = GameController.getActivePlayer();
+		HTMLPlayer.innerHTML = `<h3>Player ${activePlayer.getName()}'s turn (${activePlayer.getMark()})</h3>`;
+	};
 
-            const boardItem = document.createElement('button');
-            boardItem.classList.add('board-item');
-            boardItem.setAttribute('data-index', currentIndex);
+	const showWinner = () => {
+		console.log(`Player ${GameController.getActivePlayer().getName()} won!!`);
+		removeEventListener();
+	};
 
-            switch(element) {
-                case undefined: 
-                    break;
+	const showDraw = () => {
+		console.log("It's a draw!!");
+		removeEventListener();
+	};
 
-                case 'x':
-                    boardItem.innerHTML = '<p class="mark">X</p>';
-                    break;
-                
-                case 'o':
-                    boardItem.innerHTML = '<p class="mark">O</p>'
-                    break;
-            }
+	const removeEventListener = () => {
+		const boardItems = document.querySelectorAll(".board-item");
+		boardItems.forEach((item) => {
+			item.removeEventListener("click", GameController.playerMove);
+		});
+	};
 
-            gameboard.append(boardItem);
-            boardItem.addEventListener('click', GameController.playRound)
-            currentIndex++;
+	restartBtn.addEventListener("click", GameController.restartGame);
 
+	// First call
+	initializeHTML();
 
-
-        });
-    }
-
-    const removeEventListeners = () => {
-        const boardItems = document.querySelectorAll('.board-item');
-        boardItems.forEach(item => {
-            item.removeEventListener('click', GameController.playRound);            
-        });
-    }
-    const showTurn = (player) => {
-        // Show current player's turn and mark.
-    }
-
-    const showWinner = (player) => {
-        // Show winner
-    }
-
-    updateBoardDisplay()
-    return {updateBoardDisplay, showTurn, showWinner, removeEventListeners}
+	return {
+		addMark,
+		showTurn,
+		showWinner,
+		showDraw,
+		initializeHTML,
+	};
 })();
